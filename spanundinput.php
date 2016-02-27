a29a5768301dad64fa2b243cffa5ab0cf44e4c29
Trackhe <service@trackhe.de>
2016-02-27 18:23:55 +0100

    select effekt fehlt noch.

---
+++ spanundinput.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html lang="<?php echo htmlentities($sprache); ?>">
+	<head>
+		<title>Pluto Shop</title>
+		<meta name="description" content="Trackhe.de / Webseite zum Presentieren meiner Designs und Web arbeiten / Portfolio" />
+		<meta name="creator" content="M. Nehring, Leonard">
+		<meta name="theme-color" content="#000011">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="button.css">
+	</head>
+	<body>
+	
+	<!-- Button -->
+	<button class="rot">Button (Button)</button>
+	</br></br>
+	
+	<!-- Span Button -->
+	<span class="btn rot">Button (Span)</span>
+	</br></br>
+	
+	
+	<form>
+	
+	<!-- Input type="submit" Button -->
+	<input class="rot" type="submit" value="Button (submit)">
+	</br></br>
+	<!-- Input type="button" Button -->
+    <button class="rot" type="button" id="test3">Button (Button with type)</button>
+	</br></br>
+	
+    </form>
+	<!-- Farben -->
+	<button class="rot">Button</button>
+
+	<button class="gelb">Button</button>
+	
+	<button class="grün">Button</button>
+
+	<button class="hellblau">Button</button>
+
+	<button class="dunkelblau">Button</button>
+
+	<button class="schwarz">Button</button>
+
+	<button class="orange">Button</button>
+
+	</body>
+</html>
